aed55af17d74819f0be70f93957a2ad8cfea63fd
Fred Oden <fred.oden@gmail.com>
2021-03-11 09:56:11 +0100

    Updated::Restructuration of renderer

--- geometry/renderer.go
+++ geometry/renderer.go
@@ -1,31 +1,60 @@
 package geometry
 
+type Pixel struct {
+	p3d *APoint
+	p2d *APoint
+	projected bool
+	color *AColor
+}
+
+func NewPixel(p3d *APoint, color *AColor) *Pixel { p := &Pixel{p3d: p3d, color: color, projected: false}; return p; }
+
 type Renderer struct {
 	halfWidth float64
 	halfHeight float64
-	width, height int
-	zP []AColor  // Zdepth pointers
+	width, height float64
+	camera *APoint
+	zP []*Pixel  // Zdepth pointers
+	pCache map[*APoint]*APoint
 }
 
-type point2d struct {
-	x,y,z float64;
-}
-
-func NewRenderer(width, height int) *Renderer {
-	r := &Renderer{width: width, height: height};
-	r.zP = make([]AColor, width*height);
+func NewRenderer(width, height int, camera *APoint) *Renderer {
+	r := &Renderer{width: float64(width), height: float64(height)};
+	r.zP = make([]*Pixel, width*height);
 	r.halfWidth = float64(width)/2;
 	r.halfHeight = float64(height)/2;
+	r.camera = camera;
 	return r;
 }
 
-func (renderer *Renderer) project(p *APoint, camera  *APoint) *point2d {
-	k := camera[Z]/(p[Z] + camera[Z]);
-	p2d := &point2d{
-		(renderer.halfWidth + ((p[X] - camera[X])*k + camera[X])),
-		(renderer.halfHeight - ((p[Y] - camera[Y])*k + camera[Y])),
-		p[Z],
+func (renderer *Renderer) project(p *Pixel) {
+	if  (p.projected) {
+		return;
 	}
-	return p2d;
+	camera := renderer.camera;
+	k := camera[Z]/(p.p3d[Z] + camera[Z]);
+	p.p2d = &APoint{
+		(renderer.halfWidth + ((p.p3d[X] - camera[X])*k + camera[X])),
+		(renderer.halfHeight - ((p.p3d[Y] - camera[Y])*k + camera[Y])),
+		p.p3d[Z],
+	}
+	p.projected = true;
 }
+
+func (renderer *Renderer) storePoint2d(aw []*Pixel) {
+	for i:= 0; i < len(aw); i++ {
+		if (!aw[i].projected) {
+			renderer.project(aw[i]);
+		}
+		w := aw[i].p2d;
+		if (w[X] < 0 || w[X]  > renderer.width) || (w[Y] < 0 || w[Y] > renderer.height) { continue; }
+		wP :=  int(w[X] + w[Y]*renderer.width);
+		zp := renderer.zP[wP];
+		if zp == nil || zp.p2d[Z] <= w[Z] {
+			renderer.zP[wP] = aw[i];
+		}
+	}
+}
+
+
 
